51e17d56ae62fe8c861dce6f360dc4928adcebb9
wojciechstylok <wojciech.stylok@gmail.com>
2019-11-26 12:02:16 +0100

    First commit

---
+++ StudentDB/DataAccess.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace StudentDB
+{
+    public class DataAccess
+    {
+        private static string path = @".\StudentsDB.db";
+
+        private static List<Student> students = new List<Student>();
+
+        public static List<Student> LoadFromDb()
+        {
+            var db = new SQLiteConnection(path);
+            db.CreateTable<Student>();
+            var query = db.Table<Student>();
+
+            foreach (var s in query)
+            {
+                students.Add(s);
+            }
+
+            return students;
+        }
+
+        public static void EditStudent(int index, Student stu)
+        {
+            int studentId = students[index].Id;
+            var db = new SQLiteConnection(path);
+            Student student = new Student();
+            db.CreateTable<Student>();
+            var query = db.Table<Student>().Where(s => s.Id == studentId);
+            foreach (var s in query)
+            {
+                student = s;
+            }
+            if (stu.FirstName != null)
+                student.FirstName = stu.FirstName;
+            if (stu.LastName != null)
+                student.LastName = stu.LastName;
+            if (stu.DateOfBirth != null)
+                student.DateOfBirth = stu.DateOfBirth;
+            if (stu.IndexNumber != null)
+                student.IndexNumber = stu.IndexNumber;
+
+            student.Id = studentId;
+            db.Update(student);
+            db.Close();
+        }
+
+        public static List<Student> SearchStudent(string firstName, string lastName, string indexNumber, string date)
+        {
+            List<Student> searchedStudents = new List<Student>();
+            var db = new SQLiteConnection(path);
+            db.CreateTable<Student>();
+            var query = db.Table<Student>()
+                .Where(s => s.FirstName.Contains(firstName) && s.LastName.Contains(lastName) && s.IndexNumber.Contains(indexNumber) && s.DateOfBirth.Contains(date));
+
+            foreach (var s in query)
+            {
+                searchedStudents.Add(s);
+            }
+            db.Close();
+
+            return searchedStudents;
+        }
+
+        public static void AddStudent(Student student)
+        {
+            var db = new SQLiteConnection(path);
+            db.CreateTable<Student>();
+            db.Insert(student);
+            db.Close();
+        }
+
+        public static void DeleteStudent(int index)
+        {
+            int studentId = students[index].Id;
+            var db = new SQLiteConnection(path);
+            db.CreateTable<Student>();
+            db.Execute($"delete from Student where Id = {studentId}");
+            db.Close();
+        }
+
+        public static void AddGrade(int index, string grade)
+        {
+            int studentId = students[index].Id;
+            var db = new SQLiteConnection(path);
+            Student student = new Student();
+            db.CreateTable<Student>();
+            var query = db.Table<Student>().Where(s => s.Id == studentId);
+            foreach (var s in query)
+            {
+                student = s;
+            }
+            student.Grades += grade + "  ";
+            db.Update(student);
+            db.Close();
+        }
+
+        public static string GetStudentFullName(int id)
+        {
+            int studentId = students[id].Id;
+            var db = new SQLiteConnection(path);
+            Student student = new Student();
+            db.CreateTable<Student>();
+            var query = db.Table<Student>().Where(s => s.Id == studentId);
+            foreach (var s in query)
+            {
+                student = s;
+            }
+
+            return $"{student.FirstName} {student.LastName}";
+        }
+    }
+}
--- StudentDB/MainWindow.xaml.cs
+++ StudentDB/MainWindow.xaml.cs
@@ -20,9 +20,104 @@ namespace StudentDB
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Student> students = new List<Student>();
+        static int selectedStudentIndex;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            LoadStudents();
+        }
+
+        public void LoadStudents()
+        {
+            students.Clear();
+            foreach (Student s in DataAccess.LoadFromDb())
+            {
+                students.Add(s);
+            }
+
+            DisplayStudents();
+        }
+
+        private void DisplayStudents()
+        {
+            Results.ItemsSource = null;
+            Results.ItemsSource = students;
+            Results.DisplayMemberPath = "StudentInfo";
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = FirstNameText.Text;
+            string lastName = LastNameText.Text;
+            string indexNumber = IndexNumberText.Text;
+            string dateOfBirth = DayOfBirthText.SelectedDate.ToString();
+
+            students.Clear();
+            foreach (Student s in DataAccess.SearchStudent(firstName, lastName, indexNumber, dateOfBirth))
+            {
+                students.Add(s);
+            }
+            DisplayStudents();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = FirstNameText.Text;
+            string lastName = LastNameText.Text;
+            string indexNumber = IndexNumberText.Text;
+            string dateOfBirth = DayOfBirthText.SelectedDate.ToString();
+
+            Student student = new Student();
+            if (firstName != "" && lastName != "" && indexNumber != "" && dateOfBirth != "")
+            {
+                student.FirstName = firstName;
+                student.LastName = lastName;
+                student.IndexNumber = indexNumber;
+                student.DateOfBirth = dateOfBirth;
+                DataAccess.AddStudent(student);
+            }
+            FirstNameText.Text = null;
+            LastNameText.Text = null;
+            IndexNumberText.Text = null;
+            DayOfBirthText.SelectedDate = null;
+
+            LoadStudents();
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int index = Results.SelectedIndex;
+            DataAccess.DeleteStudent(index);
+
+            students.Clear();
+            LoadStudents();
+        }
+
+        private void Results_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            AddGradeBtn.Visibility = Visibility;
+        }
+
+        public static int SelectedStudentIndex()
+        {
+            return selectedStudentIndex;
+        }
+
+        private void AddGradeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            selectedStudentIndex = Results.SelectedIndex;
+            AddGradeWindow addGradeWindow = new AddGradeWindow();
+            addGradeWindow.Show();
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            selectedStudentIndex = Results.SelectedIndex;
+            EditStudentWindow editStudentWindow = new EditStudentWindow();
+            editStudentWindow.Show();
         }
     }
 }
